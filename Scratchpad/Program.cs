8790ccea0932f36162dab4e3dfb84f722646ec94
Joseph Lejtman <lejtmany@gmail.com>
2015-06-18 14:43:30 -0400

    did stuff

--- Scratchpad/Program.cs
+++ Scratchpad/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Immutable;
 
 namespace Scratchpad
 {
@@ -10,26 +11,50 @@ namespace Scratchpad
     {
         public interface IPerson
         {
+            String FirstName { get; set; }
+            String LastName { get; set; }
+            int Age { get; set; }
             void Talk();
+            String ToString();
+
         }
         public class Parent : IPerson
         {
-
+            public int Age { get; set; }
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public Parent(String firstName, String lastName, int age = 0, String nickName = "Sneaks")
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Age = age;
+            }
             public void Talk()
             {
                 Console.WriteLine("I am a Parent");
             }
+
+            public override String ToString()
+            {
+                return String.Format("Name: {0} {1} Age: {2}", FirstName, LastName, Age);
+            }
+
         }
-        public class Child : IPerson
+        public class Child : Parent
         {
-            public virtual void Talk()
+            public Child()
+                : base("Baby", "A")
+            {
+
+            }
+            public new void Talk()
             {
                 Console.WriteLine("I am a Child");
             }
         }
         public class Brat : Child
         {
-            public override void Talk()
+            public new void Talk()
             {
                 Console.WriteLine("I am a Brat");
             }
@@ -45,6 +70,7 @@ namespace Scratchpad
         public class CollegeClass<T> : ISchoolClass<T, T> where T : IPerson
         {
             List<T> students = new List<T>();
+            Func<String, int> stringLength = s => s.Length;
 
             public IEnumerable<T> GetClassList()
             {
@@ -53,13 +79,14 @@ namespace Scratchpad
 
             public void AddStudent(T student)
             {
+                Console.WriteLine("Student added first name is {0} characters", stringLength("hello"));
                 students.Add(student);
             }
         }
 
         static void Main(string[] args)
         {
-            var parent = new Parent();
+            var parent = new Parent("John", "Doe", nickName:"007");
             var child = new Child();
             Child brat1 = new Brat();
             var brat2 = new Brat();
@@ -72,7 +99,7 @@ namespace Scratchpad
             collegeClass.AddStudent(child);
             collegeClass.AddStudent(brat1);
             collegeClass.AddStudent(brat2);
-            Console.WriteLine(String.Join("," , collegeClass.GetClassList()));
+            Console.WriteLine(String.Join(",", collegeClass.GetClassList()));
         }
     }
 }
